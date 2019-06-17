f271d5a56555c63991beb39cc2ae524a4824e3bb
杨竞颉 <j.yang19@ejm.org>
2019-06-17 23:05:25 +0200

    edit list of students

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './club_logo.png';
 import Button from '@material-ui/core/Button';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import './loadCSV';
+import './App.css';
 import csv2promo from "./loadCSV";
+import StudentsTableDialog from "./studentsTable";
 
 class App extends React.Component {
   constructor(props) {
@@ -10,13 +13,18 @@ class App extends React.Component {
 
     this.state = {
       groups: [],
+      fileName: "IB Students",
       students: [],
-      queryName: "",
+      tableOpen: false,
+      loading: false,
     };
 
     this.refUploaderCSV = React.createRef();
     this.onChooseCSV = this.onChooseCSV.bind(this);
     this.onUploadCSV = this.onUploadCSV.bind(this);
+    this.handleTableOpen = this.handleTableOpen.bind(this);
+    this.handleTableClose = this.handleTableClose.bind(this);
+    this.handleTableSave = this.handleTableSave.bind(this);
   }
 
   onChooseCSV() {
@@ -24,14 +32,45 @@ class App extends React.Component {
   }
 
   onUploadCSV(e) {
-    let file = e.target.files[0];
-    let reader = new FileReader();
-    reader.onload = function(evt) {
-      let res = evt.target.result;
-      // csv2promo(res);
-      console.log(csv2promo(res));
-    };
-    reader.readAsText(file);
+    try {
+      let file = e.target.files[0];
+      let reader = new FileReader();
+      reader.onload = (evt) => {
+        let content = evt.target.result;
+        let students = csv2promo(content);
+        this.setState({
+          students,
+          fileName: file.name.split('.').slice(0, -1).join('.')
+        });
+      };
+      reader.readAsText(file);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  handleTableOpen() {
+    this.setState({tableOpen: true});
+  }
+
+  handleTableClose() {
+    this.setState({
+      tableOpen: false,
+      loading: true
+    });
+    setTimeout(
+        () => {
+          this.setState({
+            loading: false
+          });
+        },
+        500
+    );
+  }
+
+  handleTableSave(students) {
+    this.setState({students});
+    this.handleTableClose();
   }
 
   render() {
@@ -46,12 +85,28 @@ class App extends React.Component {
                  accept="text/csv"
                  onChange={this.onUploadCSV}
                  style={{display: "none"}}/>
-          <Button variant="contained"
+          <Button variant="outlined"
                   color="primary"
-                  onClick={this.onChooseCSV}
-                  className="uploadList">
+                  disabled={this.state.loading}
+                  onClick={this.onChooseCSV}>
             Upload List of Students
           </Button>
+          <Button variant="outlined"
+                  color="secondary"
+                  disabled={this.state.students.length === 0 || this.state.loading}
+                  onClick={this.handleTableOpen}>
+            Edit List of Students
+          </Button>
+          <div>
+            <LinearProgress className={this.state.loading ? '' : 'invisible'}/>
+          </div>
+          <StudentsTableDialog key={JSON.stringify(this.state.students + this.state.tableOpen)}
+                               students={this.state.students}
+                               fileName={this.state.fileName}
+                               tableOpen={this.state.tableOpen}
+                               handleTableOpen={this.handleTableOpen}
+                               handleTableClose={this.handleTableClose}
+                               handleTableSave={this.handleTableSave}/>
         </div>
     );
   }
---
+++ src/studentsTable.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CloseIcon from '@material-ui/icons/Close';
+import Dialog from '@material-ui/core/Dialog';
+import IconButton from "@material-ui/core/IconButton";
+import MaterialTable from 'material-table';
+import Slide from '@material-ui/core/Slide';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import './studentsTable.css';
+
+function encodeData(data) {
+  return data.map(s => {
+    return {
+      ...s,
+      gender: s.gender === "M" ? 0 : s.gender === "F" ? 1 : 2,
+      leader: s.leader === undefined || s.leader ? 0 : 1,
+      bio: s.bio === "HL" ? 2 : s.bio === "SL" ? 1 : 0,
+      chm: s.chm === "HL" ? 2 : s.chm === "SL" ? 1 : 0,
+      phy: s.phy === "HL" ? 2 : s.phy === "SL" ? 1 : 0,
+    }
+  });
+}
+
+const genderLookup = {0: "M", 1: "F", 2: "Other"};
+const leadershipLookup = {0: "", 1: "Yes"};
+const sciLookup = {0: " ", 1: "SL", 2: "HL"};
+
+function decodeData(data) {
+  return data.map(s => {
+    return {
+      ...s,
+      gender: genderLookup[s.gender],
+      leader: leadershipLookup[s.leader],
+      bio: sciLookup[s.bio],
+      chm: sciLookup[s.chm],
+      phy: sciLookup[s.phy],
+    }
+  });
+}
+
+class StudentsTableDialog extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.tableRef = React.createRef();
+  }
+
+  render() {
+    const Transition = React.forwardRef(function Transition(props, ref) {
+      return <Slide direction="up" ref={ref} {...props} />;
+    });
+    return <Dialog fullScreen
+                   open={this.props.tableOpen}
+                   onClose={this.props.handleTableClose}
+                   TransitionComponent={Transition}>
+      <Toolbar className={'tbl-app_bar'}>
+        <IconButton edge="start" color="inherit" onClick={this.props.handleTableClose} href="#" aria-label="Close">
+          <CloseIcon/>
+        </IconButton>
+        <Typography variant="h5" className="tbl-title">
+          List of Students
+        </Typography>
+        <Button color="inherit"
+                onClick={() => this.props.handleTableSave(decodeData(this.tableRef.current.state.data))}>
+          Save
+        </Button>
+      </Toolbar>
+      <StudentsTable
+          ref={this.tableRef}
+          students={this.props.students}
+          fileName={this.props.fileName}/>
+    </Dialog>
+  }
+}
+
+
+class StudentsTable extends React.Component {
+  TIMEOUT = 230;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      columns: [
+        {title: "Name", field: "name"},
+        {title: "Gender", field: "gender", lookup: genderLookup},
+        {title: "Leadership", field: "leader", lookup: leadershipLookup},
+        {title: "Biology level", field: "bio", lookup: sciLookup},
+        {title: "Chemistry level", field: "chm", lookup: sciLookup},
+        {title: "Physics level", field: "phy", lookup: sciLookup},
+      ],
+      data: encodeData(props.students),
+    };
+  }
+
+  render() {
+    return <MaterialTable
+        title={this.props.fileName}
+        columns={this.state.columns}
+        data={this.state.data}
+        options={{
+          exportButton: true,
+          filtering: true,
+          pageSize: this.state.data.length,
+        }}
+        editable={{
+          onRowAdd: newData =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  {
+                    const data = this.state.data;
+                    data.push(newData);
+                    this.setState({data}, () => resolve());
+                  }
+                  resolve()
+                }, this.TIMEOUT)
+              }),
+          onRowUpdate: (newData, oldData) =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  {
+                    const data = this.state.data;
+                    const index = data.indexOf(oldData);
+                    data[index] = newData;
+                    this.setState({data}, () => resolve());
+                  }
+                  resolve()
+                }, this.TIMEOUT)
+              }),
+          onRowDelete: oldData =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  {
+                    let data = this.state.data;
+                    const index = data.indexOf(oldData);
+                    data.splice(index, 1);
+                    this.setState({data}, () => resolve());
+                  }
+                  resolve()
+                }, this.TIMEOUT)
+              }),
+        }}
+    />
+  }
+}
+
+export default StudentsTableDialog;
