b7fc794622c0632b54d5809cf021fda7cd080f49
杨竞颉 <j.yang19@ejm.org>
2019-06-19 22:57:14 +0200

    wtf window.crypto

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,7 @@
+import {sha256} from 'js-sha256';
+
 async function hash(s) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(s);
-  const buffer = await window.crypto.subtle.digest('SHA-256', data);
-  return hexString(buffer);
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#Converting_a_digest_to_a_hex_string
-function hexString(buffer) {
-  const byteArray = new Uint8Array(buffer);
-
-  const hexCodes = [...byteArray].map(value => {
-    const hexCode = value.toString(16);
-    const paddedHexCode = hexCode.padStart(2, '0');
-    return paddedHexCode;
-  });
-
-  return hexCodes.join('');
+  return sha256(s);
 }
 
 export default hash;
